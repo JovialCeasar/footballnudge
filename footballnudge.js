d75daa140d35178b2a20a337994153970cbfb787
JovialCeasar <anthonybeilin@gmail.com>
2017-07-31 23:17:14 +0100

    Added Call

---
+++ footballnudge.js
@@ -0,0 +1,6 @@
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "GET https://soccer.sportmonks.com/api/v2.0/odds/inplay/fixture/id}?api_token=pjHVFVI5vM6oMhP4F3mw07qEKUvzJ9kTLvCYvUEhnx6TCrN8j2hxeWoRtXp3", false);
+xhr.send();
+
+console.log(xhr.status);
+console.log(xhr.statusText);
