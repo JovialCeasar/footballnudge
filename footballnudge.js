252a0be2d5f7fb3a0a1142c0359a4e26b293fd9b
JovialCeasar <anthonybeilin@gmail.com>
2017-08-01 08:37:01 +0100

    Version 2
    Test

--- footballnudge.js
+++ footballnudge.js
@@ -1,6 +1,8 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://soccer.sportmonks.com/api/v2.0/odds/inplay/fixture/id}?api_token=pjHVFVI5vM6oMhP4F3mw07qEKUvzJ9kTLvCYvUEhnx6TCrN8j2hxeWoRtXp3", false);
-xhr.send();
 
-console.log(xhr.status);
-console.log(xhr.statusText);
+function UserAction() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "https://soccer.sportmonks.com/api/v2.0/odds/inplay/fixture/id}?api_token=pjHVFVI5vM6oMhP4F3mw07qEKUvzJ9kTLvCYvUEhnx6TCrN8j2hxeWoRtXp3", false);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send();
+    var response = JSON.parse(xhttp.responseText);
+}
